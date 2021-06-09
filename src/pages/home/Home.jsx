e7d1f2951e0014173fba61e40e91a6e4f250b50d
zzg <zhouzhigang@bbdservice.com>
2021-06-09 11:33:42 +0800

    feat:adjust some css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,13 +7,21 @@ export default function Home() {
 
     return (
         <div className={styles.home}>
-            <div className={styles.componentsPickingPanel}>
-                <ComponentPickingPanel />
+            <div className={styles.header}>
+                布局快速生成工具
             </div>
-            <div className={styles.displayLayoutPanel}>
-                <DisplayLayoutPanel />
+            <div className={styles.workBench}>
+                <div className={styles.componentsPickingPanel}>
+                    <ComponentPickingPanel />
+                </div>
+                <div className={styles.displayLayoutPanel}>
+                    <DisplayLayoutPanel />
+                </div>
+                <div className={styles.editingPanel}></div>
             </div>
-            <div className={styles.editingPanel}></div>
+            <div className={styles.footer}>
+                拖动快速生成页面布局和组件，请使用1920宽度的屏幕操作
+            </div>
         </div>
     );
 }
